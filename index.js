0c1ee4c081c3319f36c79bb210e31a2fa2a23874
kiraboannet624 <kiraboannet624@gmail.com>
2026-02-17 15:05:45 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// exerise 1
+const productCategories = ["Books", "Electronics", "clothing Home" , "Kitche Toys $ Games"]
+//  a
+console.log (productCategories[0]);
+// b
+console.log(productCategories[productCategories.length - 1]);
+// c
+console.log(productCategories.length);
+// d
+productCategories[1] = "pen";
+// e
+console.log(productCategories);
+
