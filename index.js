9c3d99a1695802b586379d68cd961be8494b8cb8
kiraboannet624 <kiraboannet624@gmail.com>
2026-02-17 16:12:54 +0200

    exercise2,3,4

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ console.log(productCategories.length);
 productCategories[1] = "pen";
 // e
 console.log(productCategories);
+// exerise3
+const originalScores = [90, 85, 78];
+// b
+const referenceCopy = originalScores;
+console.log(referenceCopy);
+// c
+referenceCopy[0] = 100;
+console.long("reference Copy :", referenceCopy);
+// d
+const spreadCopy = [...originalScores] // creates a new array with the same elements
+//e
+spreadCopy[0]= 50;
+console.log("Original Scores:", originalScores);
+console.log("Spread Copy:", spreadCopy);
+ 
+
+// section 3 seaching $ checking 
+// execise4 :finding elements by index and value
+
+const studentNames= ["Alice", "Bob", "Charlie", "Alice", "David"];
+
+//a 
+console.log(studentNames.indexOf("Alice"));
+//b
+console.log(studentNames.lastIndexOf("Alice"));
+
+
 
