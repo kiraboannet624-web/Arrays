02314267d9d6ef981635c62e3dda3a9f0f8bbec3
kiraboannet624 <kiraboannet624@gmail.com>
2026-02-18 16:50:42 +0200

    exercise2

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ console.log(productCategories.length); // Output: 5
 productCategories[1] = "pen"
 // e
 console.log(productCategories); // Output: ["Books", "pen", "Clothing", "Home & Kitchen", "Toys & Games"]
+//Exercise 2:  Push, pop, shift, unshift, splice
+ 
+ let invetory = ["10", "20", "30"];
+ // a
+ invetory.push(40);
+ console.log(invetory); // Output: ["10", "20", "30", 40];
+    // b
+invetory.pop();
+console.log(invetory); // Output: ["10", "20", "30"]
+    // c
+invetory.unshift(0);
+console.log(invetory);// Output: [0, "10", "20", "30"]
+// d
+invetory.shift();
+console.log(invetory); // Output: ["10", "20", "30"]
+    // e
+invetory.splice(1, 1, 25, 35);
+console.log(invetory); // Output: ["10", "25", "35"]
+
